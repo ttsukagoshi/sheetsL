5c337d7b1fb406147ac4612a5232fe6e498e81ea
ttsukagoshi <taro.tsukagoshi@gmail.com>
2023-03-12 00:16:52 +0900

    Create sheetsl.ts to replace index.js (#30)

--- src/sheetsl.ts
+++ src/sheetsl.ts
@@ -1,5 +1,4 @@
-/**
- * Copyright 2022 TSUKAGOSHI Taro
+/* Copyright 2022 TSUKAGOSHI Taro
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,16 +14,46 @@
  */
 
 const ADDON_NAME = 'SheetsL';
-const UP_KEY_DEEPL_API_KEY = 'deeplApiKey';
-const UP_KEY_SOURCE_LOCALE = 'sourceLocale';
-const UP_KEY_TARGET_LOCALE = 'targetLocale';
-const DEEPL_API_VERSION = 'v2';
-const DEEPL_API_BASE_URL = `https://api-free.deepl.com/${DEEPL_API_VERSION}/`;
+const UP_KEY_DEEPL_API_KEY = 'deeplApiKey'; // User property key for saving the DeepL API key
+const UP_KEY_SOURCE_LOCALE = 'sourceLocale'; // User property key for saving the source language for DeepL
+const UP_KEY_TARGET_LOCALE = 'targetLocale'; // User property key for saving the target language for DeepL
+const DEEPL_API_VERSION = 'v2'; // DeepL API version
+const DEEPL_API_BASE_URL_FREE = `https://api-free.deepl.com/${DEEPL_API_VERSION}/`;
+const DEEPL_API_BASE_URL_PRO = `https://api.deepl.com/${DEEPL_API_VERSION}/`;
+
+/**
+ * The JavaScript object of a DeepL-supported language.
+ * GET request on /v2/languages returns an array of this object.
+ * @see https://www.deepl.com/docs-api/general/get-languages/
+ */
+interface DeepLSupportedLanguages {
+  language: string;
+  name: string;
+  supports_formality: boolean;
+}
+
+/**
+ * The response from the DeepL API for POST /v2/translate.
+ * @see https://www.deepl.com/docs-api/translate-text/
+ */
+interface DeepLTranslationResponse {
+  translations: DeepLTranslationObj[];
+}
+
+/**
+ * The individual translated text object in the translated response
+ * from DeepL API.
+ * @see https://www.deepl.com/docs-api/translate-text/
+ */
+interface DeepLTranslationObj {
+  detected_source_language: string;
+  text: string;
+}
 
 /**
  * Create add-on menu on opening spreadsheet file.
  */
-function onOpen() {
+function onOpen(): void {
   const ui = SpreadsheetApp.getUi();
   ui.createAddonMenu()
     .addSubMenu(
@@ -43,14 +72,14 @@ function onOpen() {
 /**
  * Create add-on menu on installation of add-on.
  */
-function onInstall() {
+function onInstall(): void {
   onOpen();
 }
 
 /**
  * Store DeepL API authentication key in user property.
  */
-function setDeeplAuthKey() {
+function setDeeplAuthKey(): void {
   const ui = SpreadsheetApp.getUi();
   try {
     const promptResponse = ui.prompt(
@@ -76,14 +105,15 @@ function setDeeplAuthKey() {
       `[${ADDON_NAME}] Completed: Your DeepL API Authentication Key has been saved as a user property.`
     );
   } catch (error) {
-    ui.alert(error.stack);
+    console.error(error.stack);
+    ui.alert(error.message);
   }
 }
 
 /**
  * Delete the stored DeepL API authentication key in user property.
  */
-function deleteDeeplAuthKey() {
+function deleteDeeplAuthKey(): void {
   const ui = SpreadsheetApp.getUi();
   try {
     PropertiesService.getUserProperties().deleteProperty(UP_KEY_DEEPL_API_KEY);
@@ -91,14 +121,15 @@ function deleteDeeplAuthKey() {
       `[${ADDON_NAME}] Complete: DeepL API Authentication Key has been deleted from your user property.`
     );
   } catch (error) {
-    ui.alert(error.stack);
+    console.error(error.stack);
+    ui.alert(error.message);
   }
 }
 
 /**
  * Set source and target languages for translation.
  */
-function setLanguage() {
+function setLanguage(): void {
   const ui = SpreadsheetApp.getUi();
   try {
     const up = PropertiesService.getUserProperties();
@@ -123,7 +154,10 @@ function setLanguage() {
     // Source language
     const promptSourceLocale = ui.prompt(
       `In which language is the source text written? Enter the two-letter language code. Leave this value empty to use DeepL's language auto detection.\n\nAvailable values:\n${availableLocaleSource
-        .map((langObj) => `- ${langObj.language} (${langObj.name})`)
+        .map(
+          (langObj: DeepLSupportedLanguages) =>
+            `- ${langObj.language} (${langObj.name})`
+        )
         .join('\n')}`,
       ui.ButtonSet.OK_CANCEL
     );
@@ -137,7 +171,7 @@ function setLanguage() {
       .toUpperCase();
     if (
       !availableLocaleSource
-        .map((langObj) => langObj.language)
+        .map((langObj: DeepLSupportedLanguages) => langObj.language)
         .includes(responseSourceLocale) &&
       responseSourceLocale != ''
     ) {
@@ -148,7 +182,10 @@ function setLanguage() {
     // Target language
     const promptTargetLocale = ui.prompt(
       `Into which language should the text be translated? Enter the two- or four-letter language code.\n\nAvailable values:\n${availableLocaleTarget
-        .map((langObj) => `- ${langObj.language} (${langObj.name})`)
+        .map(
+          (langObj: DeepLSupportedLanguages) =>
+            `- ${langObj.language} (${langObj.name})`
+        )
         .join('\n')}`,
       ui.ButtonSet.OK_CANCEL
     );
@@ -162,7 +199,7 @@ function setLanguage() {
       .toUpperCase();
     if (
       !availableLocaleTarget
-        .map((langObj) => langObj.language)
+        .map((langObj: DeepLSupportedLanguages) => langObj.language)
         .includes(responseTargetLocale)
     ) {
       throw new Error(
@@ -177,11 +214,12 @@ function setLanguage() {
     // Complete
     ui.alert('Completed: Language setting has been completed.');
   } catch (error) {
-    ui.alert(error.stack);
+    console.error(error.stack);
+    ui.alert(error.message);
   }
 }
 
-function translateRange() {
+function translateRange(): void {
   const ui = SpreadsheetApp.getUi();
   const activeSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   const selectedRange = activeSheet.getActiveRange();
@@ -227,21 +265,26 @@ function translateRange() {
     // Complete
     ui.alert('Complete: Translation has been completed.');
   } catch (error) {
-    ui.alert(error.stack);
+    console.error(error.stack);
+    ui.alert(error.message);
   }
 }
 
 /**
  * Call the DeepL API on the `translate` endpoint
- * @param {string | string[]} sourceText Array of texts to translate
- * @param {string} sourceLocale The language of the source text
- * @param {string} targetLocale The language to be translated into
- * @returns {string | string[]} Array of translated texts.
+ * @param sourceText Array of texts to translate
+ * @param sourceLocale The language of the source text
+ * @param targetLocale The language to be translated into
+ * @returns Array of translated texts.
  * @see https://www.deepl.com/docs-api/translate-text/
  */
-function deepLTranslate(sourceText, sourceLocale, targetLocale) {
+function deepLTranslate(
+  sourceText: string | string[],
+  sourceLocale: string,
+  targetLocale: string
+): string[] {
   const endpoint = 'translate';
-  let sourceTextCasted;
+  let sourceTextCasted: string;
   if (!sourceText || sourceText.length === 0) {
     throw new Error(`[${ADDON_NAME}] Empty input.`);
   }
@@ -260,13 +303,7 @@ function deepLTranslate(sourceText, sourceLocale, targetLocale) {
       `[${ADDON_NAME}] API Key Unavailable: Set the DeepL API Authentication Key from the Settings > Set Auth Key of the add-on menu.`
     );
   }
-  let baseUrl = DEEPL_API_BASE_URL;
-  if (!apiKey.endsWith(':fx')) {
-    // Auth keys of DeepL API Free end with ':fx'
-    // API domain differs for DeepL Free API and DeepL Pro API
-    // See https://support.deepl.com/hc/en-us/articles/360021183620-DeepL-API-Free-vs-DeepL-API-Pro
-    baseUrl = baseUrl.replace('api-free.deepl.com', 'api.deepl.com ');
-  }
+  let baseUrl = getDeepLApiBaseUrl(apiKey);
   // Call the DeepL API
   let url =
     baseUrl +
@@ -292,8 +329,11 @@ function deepLTranslate(sourceText, sourceLocale, targetLocale) {
       `[${ADDON_NAME}] Error on Calling DeepL API: ${response.getContentText()}`
     );
   }
-  const translatedText = JSON.parse(response.getContentText()).translations.map(
-    (translationsResponse) => translationsResponse.text
+  const translatedTextObj: DeepLTranslationResponse = JSON.parse(
+    response.getContentText()
+  );
+  const translatedText: string[] = translatedTextObj.translations.map(
+    (translationsResponse: DeepLTranslationObj) => translationsResponse.text
   );
   return translatedText;
 }
@@ -301,11 +341,11 @@ function deepLTranslate(sourceText, sourceLocale, targetLocale) {
 /**
  * Retrieve the list of languages that are currently supported for translation,
  * either as source or target language.
- * @param {string} type Sets whether source or target languages should be listed. Takes either `source` or `target`.
- * @returns
+ * @param type Sets whether source or target languages should be listed. Takes either `source` or `target`.
+ * @returns An array of the supported languages.
  * @see https://www.deepl.com/docs-api/general/get-languages/
  */
-function deepLGetLanguages(type = 'source') {
+function deepLGetLanguages(type = 'source'): DeepLSupportedLanguages[] {
   const endpoint = 'languages';
   // API key
   const apiKey =
@@ -315,13 +355,7 @@ function deepLGetLanguages(type = 'source') {
       `[${ADDON_NAME}] API Key Unavailable: Set the DeepL API Authentication Key from the Settings > Set Auth Key of the add-on menu.`
     );
   }
-  let baseUrl = DEEPL_API_BASE_URL;
-  if (!apiKey.endsWith(':fx')) {
-    // Auth keys of DeepL API Free end with ':fx'
-    // API domain differs for DeepL Free API and DeepL Pro API
-    // See https://support.deepl.com/hc/en-us/articles/360021183620-DeepL-API-Free-vs-DeepL-API-Pro
-    baseUrl = baseUrl.replace('api-free.deepl.com', 'api.deepl.com ');
-  }
+  let baseUrl = getDeepLApiBaseUrl(apiKey);
   // Call the DeepL API
   let url = baseUrl + endpoint + `?auth_key=${apiKey}&type=${type}`;
   const response = UrlFetchApp.fetch(url, { muteHttpExceptions: true });
@@ -330,6 +364,19 @@ function deepLGetLanguages(type = 'source') {
   } else {
     throw new Error(`[${ADDON_NAME}] ${response.getContentText()}`);
   }
+}
+
+/**
+ * Returns the DeepL API base URL. The URL depends on whether the user's account
+ * is DeepL API Free or Pro. Auth keys of DeepL API Free end with `:fx`
+ * @param apiKey The DeepL API Free/Pro Authentication Key
+ * @returns The relevant base URL for DeepL API
+ * @see https://support.deepl.com/hc/en-us/articles/360021183620-DeepL-API-Free-vs-DeepL-API-Pro
+ */
+function getDeepLApiBaseUrl(apiKey: string): string {
+  return apiKey.endsWith(':fx')
+    ? DEEPL_API_BASE_URL_FREE
+    : DEEPL_API_BASE_URL_PRO;
 }
 
 if (typeof module === 'object') {
