576659ca0dad6f15691c722cadbe8ca389dc562e
ttsukagoshi <taro.tsukagoshi@gmail.com>
2023-03-25 21:29:31 +0900

    Add test for getDeepLApiKey to check whether error is properly thrown

---
+++ __tests__/getDeepLApiKey.test.ts
@@ -0,0 +1,19 @@
+import { ADDON_NAME, getDeepLApiKey } from '../src/sheetsl';
+
+PropertiesService.getUserProperties = jest.fn(() => ({
+  getProperty: jest.fn(() => undefined),
+})) as any;
+
+const testObj = {
+  title:
+    'Case when DeepL API Authentication Key is not saved in the user property',
+  errorMessage: `[${ADDON_NAME}] API Key Unavailable: Set the DeepL API Authentication Key from the Settings > Set Auth Key of the add-on menu.`,
+};
+
+describe('getDeepLApiKey Error', () => {
+  test(testObj.title, () => {
+    expect(() => {
+      getDeepLApiKey();
+    }).toThrowError(new Error(testObj.errorMessage));
+  });
+});
