0538727d5d84d2dd8f9b8a80486780d735e8f43f
ttsukagoshi <taro.tsukagoshi@gmail.com>
2024-02-09 03:45:11 +0900

    Add `coverageThreshold` to `jest.config.js`

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,14 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   globals: {
     PropertiesService: {},
     SpreadsheetApp: {},
